8a3e95f431c7cb1d0cdafed768307b67ee3adf35
FernandoAlonsoCalderon <118294399+FernandoAlonsoCalderon@users.noreply.github.com>
2024-01-17 22:45:40 +0100

    Update barra Lateral scripts.js

--- Practica 3/public/js/scripts.js
+++ Practica 3/public/js/scripts.js
@@ -15,3 +15,35 @@ async function cargarMas() {
   cargarMasAsignaturas++;
   setupCheckboxFunctionality();
 }
+
+function setupCheckboxFunctionality() {
+  // Obtén todos los elementos con la clase 'container2'
+  const checkboxes = document.querySelectorAll('.container2 input[type="checkbox"]');
+
+  // Cargar valores almacenados en localStorage
+  checkboxes.forEach(checkbox => {
+    const id = checkbox.closest('.aline').id;
+    const storedValue = localStorage.getItem(`checkbox_${id}`);
+    
+    if (storedValue) {
+      const { isChecked, title } = JSON.parse(storedValue);
+      checkbox.checked = isChecked;
+      checkbox.setAttribute('data-title', title);
+    }
+
+    // Agrega un event listener para detectar cambios en los checkboxes
+    checkbox.addEventListener('change', function () {
+      const isChecked = this.checked;
+      const id = this.closest('.aline').id;
+      const title = this.closest('.portfolio-caption').querySelector('.portfolio-caption-heading').innerText;
+
+      // Almacena el valor en localStorage
+      localStorage.setItem(`checkbox_${id}`, JSON.stringify({ isChecked, title }));
+
+      // Limpia las entradas no pulsadas en true
+      cleanLocalStorage();
+
+      // Actualiza los títulos 
+      cargarYMostrarTitulos();
+    });
+  });
